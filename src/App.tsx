5feb43785d32f0a03865da3e252345bb95975f9d
Slava <110696118+sl7one@users.noreply.github.com>
2023-12-23 16:08:20 +0200

    add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Container from './components/Container'
+import Form from './components/Form'
 
 function App() {
   return (
     <Container>
-      <div>hello</div>
+      <Form/>
     </Container>
   )
 }
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,22 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+export default function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form
+      className="w-full max-w-screen-sm mx-auto p-6 border-2 border-indigo-600 rounded-xl flex gap-4 flex-col mt-8"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <button type="button">submit</button>
+    </form>
+  );
+}
